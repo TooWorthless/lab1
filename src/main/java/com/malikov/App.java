e1f0b10515fb8aa5f73cfc98a91faba7746bbbf6
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2023-09-20 14:42:52 +0300

    Fixed maven config. Created test book obj in App.java

--- src/main/java/com/malikov/App.java
+++ src/main/java/com/malikov/App.java
@@ -1,10 +1,13 @@
 package com.malikov;
 
+import com.malikov.book.Book;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Book myBook = new Book(87362487, "Nameasdasd", "Vladik", 2003);
+
+        System.out.println(myBook.toString());
     }
 }
